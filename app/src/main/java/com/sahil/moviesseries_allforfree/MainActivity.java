7f16c0dc6979eedbfd091986d50d52921f942bc0
Sahil Singh <sahilsingh08062001@gmail.com>
2021-07-11 00:45:39 +0530

    ui

--- app/src/main/java/com/sahil/moviesseries_allforfree/MainActivity.java
+++ app/src/main/java/com/sahil/moviesseries_allforfree/MainActivity.java
@@ -2,15 +2,20 @@ package com.sahil.moviesseries_allforfree;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView3,textView5,textView6;
     FloatingActionButton next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +28,41 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,Web.class));
             }
         });
+    }
+    public void rateApp(View view)
+    {
+        try
+        {
+            Intent rateIntent = rateIntentForUrl("https://play.google.com/store/apps/details?id=com.sahil.number&hl=en_IN&gl=US");
+            startActivity(rateIntent);
+        }
+        catch (ActivityNotFoundException e)
+        {
+            Intent rateIntent = rateIntentForUrl("https://play.google.com/store/apps/details");
+            startActivity(rateIntent);
+        }
+    }
+
+    private Intent rateIntentForUrl(String url)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("%s?id=%s", url, getPackageName())));
+        int flags = Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_MULTIPLE_TASK;
+        if (Build.VERSION.SDK_INT >= 21)
+        {
+            flags |= Intent.FLAG_ACTIVITY_NEW_DOCUMENT;
+        }
+        else
+        {
+            //noinspection deprecation
+            flags |= Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET;
+        }
+        intent.addFlags(flags);
+        return intent;
+    }
+
+    public void contact(View view) {
+    }
+
+    public void share(View view) {
     }
 }
